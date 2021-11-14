d4acdf1fde48b451b459be8679a03f0ee435512e
Your Name <you@example.com>
2021-11-14 22:19:43 +0700

    fisrt commit

---
+++ 11-fs-async.js
@@ -0,0 +1,32 @@
+const { readFileSync, writeFileSync, writeFile, readFile } = require('fs')
+
+
+console.log('start')
+readFile('./content/first.txt', 'utf-8', (err, result) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    const first = result;
+    readFile('./content/first.txt', 'utf-8', (err, result) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        const second = result;
+
+        writeFile(
+            './content/result-async.txt',
+            `Here is the result : ${first}, ${second}`,
+            (err, result) => {
+                if (err) {
+                    console.log(err)
+                    return
+                }
+                console.log('done with this task')
+            }
+        )
+    })
+})
+
+console.log('starting the next task')
